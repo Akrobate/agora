08cfe26099566ace09ed9b49c6d692627b1320b7
Akrobate <fedorov.artiom@gmail.com>
2021-08-06 04:02:45 +0200

    Added test for Guest accesses

---
+++ backend/test/functional/GuestUserCampaignAccess.functional.test.js
@@ -0,0 +1,135 @@
+'use strict';
+
+const superTest = require('supertest');
+const HTTP_CODE = require('http-status');
+
+const {
+    v4,
+} = require('uuid');
+
+const {
+    expect,
+} = require('chai');
+
+const {
+    app,
+} = require('../../src/app');
+
+const {
+    DataSeeder,
+} = require('../helpers/DataSeeder');
+
+const superApp = superTest(app);
+
+describe('CampaignAccess', () => {
+
+    const manager_user_seed = {
+        id: 100,
+        password: 'ShouldHaveLettersDigitsAndAtLeast8chars1',
+        email: 'manager.user@test.com',
+    };
+
+    const campaign_seed = {
+        id: 10,
+        title: 'Title of campaignsss',
+        description: 'Somethings',
+        owner_user_id: manager_user_seed.id,
+    };
+
+    const manager_campaign_user_seed = {
+        campaign_id: campaign_seed.id,
+        user_id: 100,
+        access_level: 3,
+    };
+
+    const guest_user_seed = {
+        id: 200,
+        email: 'guest.user@test.com',
+    };
+
+    const guest_campaign_user_seed = {
+        campaign_id: campaign_seed.id,
+        user_id: guest_user_seed.id,
+        public_token: '8185933f78c749b381ad630308cd1257',
+        access_level: 1,
+    };
+
+
+    before(async () => {
+
+        await DataSeeder.truncate('CampaignRepository');
+        await DataSeeder.truncate('CampaignUserRepository');
+        await DataSeeder.truncate('UserRepository');
+
+        await DataSeeder.createUserHashPassword(manager_user_seed);
+        await DataSeeder.create('CampaignRepository', campaign_seed);
+        await DataSeeder.create('CampaignUserRepository', manager_campaign_user_seed);
+
+        await DataSeeder.create('UserRepository', guest_user_seed);
+        await DataSeeder.create('CampaignUserRepository', guest_campaign_user_seed);
+
+    });
+
+
+    it('Manager should be able to add a guest user', async () => {
+        await superApp
+            .post(`/api/v1/campaigns/${campaign_seed.id}/users`)
+            .set('Authorization', `Bearer ${DataSeeder.getJwtFullAccessToken(manager_user_seed)}`)
+            .send({
+                email: 'artiom.somebody@test.com',
+                access_level: 1,
+            })
+            .expect(HTTP_CODE.CREATED)
+            .expect((response) => {
+                expect(response.body).to.have.property('campaign_id', '10');
+                expect(response.body).to.have.property('access_level', 1);
+            });
+    });
+
+
+    it('Guest user should not be able add a guest user', async () => {
+        await superApp
+            .post(`/api/v1/campaigns/${campaign_seed.id}/users`)
+            .set('Authorization', `Bearer ${DataSeeder.getJwtGuestAccessToken(guest_user_seed)}`)
+            .send({
+                email: `${v4()}.${v4()}@${v4()}.${v4()}`,
+                access_level: 1,
+            })
+            .expect(HTTP_CODE.UNAUTHORIZED)
+            .expect((response) => {
+                expect(response.body).to.have.property('message', 'Guest user cannot add create users to campaigns');
+            });
+    });
+
+
+    it('Guest user should not be able to create a campaign', async () => {
+        await superApp
+            .post('/api/v1/campaigns')
+            .set('Authorization', `Bearer ${DataSeeder.getJwtGuestAccessToken(guest_user_seed)}`)
+            .send({
+                title: v4(),
+                description: v4(),
+            })
+            .expect(HTTP_CODE.UNAUTHORIZED)
+            .expect((response) => {
+                expect(response.body).to.have.property('message', 'Guest user cannot create campaigns');
+            });
+    });
+
+
+    it('Guest user should be able to login', async () => {
+        await superApp
+            .post('/api/v1/users/login/guest')
+            .send({
+                public_token: guest_campaign_user_seed.public_token,
+            })
+            .expect(HTTP_CODE.OK)
+            .expect((response) => {
+                expect(response.body).to.have.property('token');
+                expect(response.body.token)
+                    .to.equal(DataSeeder.getJwtGuestAccessToken(guest_user_seed));
+            });
+    });
+
+
+});
