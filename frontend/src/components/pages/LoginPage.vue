dde6c19292be2cf90442d9ebf6078f7af0d4fbba
Akrobate <fedorov.artiom@gmail.com>
2021-08-11 03:34:57 +0200

    Added Login and register pages

---
+++ frontend/src/components/pages/LoginPage.vue
@@ -0,0 +1,126 @@
+<template>
+  <v-container
+    class="fill-height"
+    fluid
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        cols="12"
+        sm="8"
+        md="4"
+      >
+        <v-card class="elevation-6">
+          <v-toolbar
+            color="primary"
+            dark
+            flat
+          >
+            <v-toolbar-title>Connexion <span v-if="app_version" >v{{ app_version }}</span></v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="Email"
+                name="email"
+                v-model="email"
+                prepend-icon="mdi-account"
+                type="text"
+              />
+
+              <v-text-field
+                id="password"
+                label="Password"
+                name="password"
+                v-model="password"
+                prepend-icon="mdi-key"
+                type="password"
+              />
+            </v-form>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer />
+
+            <router-link class="mr-5" :to="{ name: 'register'}">Créer un compte</router-link>
+
+            <v-btn
+              color="primary"
+              @click="login()"
+              :loading="loading"
+              large
+            >
+              Se connecter
+            </v-btn>
+          </v-card-actions>
+
+        </v-card>
+
+        <v-snackbar
+          :timeout="10000"
+          :top="true"
+          color="error"
+          v-model="snackbar"
+        >
+          {{ snackbar_text }}
+          <v-btn
+            text
+            @click.native="snackbar = false"
+          >
+            Fermer
+          </v-btn>
+        </v-snackbar>
+
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+import { mapActions } from 'vuex'
+import {version} from '../../../package.json';
+
+export default {
+    name: 'LoginPage',
+    data() {
+        return {
+            email : "",
+            password : "",
+            loading: false,
+            snackbar: false,
+            snackbar_text: '',
+            app_version: version,
+        }
+    },
+    methods: {
+        ...mapActions({
+            authenticate: 'authentication_store/login'
+        }),
+        async login() {
+            this.loading = true
+            try {
+                await this.authenticate({
+                    email: this.email,
+                    password: this.password,
+                })
+                this.$router.push({ name: 'home' })
+            } catch (error) {
+                console.log('errorerrorerrorerror', error);
+                if (error.response.status == 401) {
+                    this.snackbar_text = 'Votre email ou mot de passe est incorrect'
+                } else {
+                    this.snackbar_text = 'Probleme technique, veuillez essayer plus tard'
+                }
+                this.email = ''
+                this.password = ''
+                this.loading = false
+                this.snackbar = true
+            }
+        },
+    },
+}
+
+</script>
