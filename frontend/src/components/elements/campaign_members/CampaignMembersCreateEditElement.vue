4b941874837e08d6c51d46225412fa658ab1fbff
Akrobate <fedorov.artiom@gmail.com>
2021-08-15 23:39:21 +0200

    Added campaign members

---
+++ frontend/src/components/elements/campaign_members/CampaignMembersCreateEditElement.vue
@@ -0,0 +1,168 @@
+<template>
+    <v-card>
+
+        <v-card-title>
+            <span class="headline">Membre</span>
+        </v-card-title>
+
+        <v-card-text>
+            <v-container>
+
+                <v-form
+                    ref="form"
+                    v-model="valid"
+                    lazy-validation
+                >
+                    <v-text-field
+                        v-model="email"
+                        :counter="255"
+                        :rules="email_rules"
+                        label="email"
+                        required
+                        :disabled="!creation_mode"
+                    ></v-text-field>
+
+                    <v-select
+                        v-model="access_level"
+                        :items="access_level_list"
+                        item-text="label"
+                        item-value="id"
+                        label="Privilèges"
+                        :rules="access_level_rules"
+                    ></v-select>
+                </v-form>
+
+                <v-switch
+                    v-model="is_participant"
+                    label="Participe a la campagne"
+                ></v-switch>
+
+                
+            </v-container> 
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                color="blue darken-1"
+                text
+                @click="reset"
+            >
+                Annuler
+            </v-btn>
+            <v-btn
+                color="blue darken-1"
+                text
+                @click="save"
+            >
+                Sauvegarder
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+
+import { mapActions } from 'vuex'
+
+export default {
+    name: 'CampaignMembersCreateEditElement',
+    props: {
+        campaign_id: Number,
+        campaign_user_id: Number,
+    },
+    data() {
+        return {
+            valid: true,
+            email: '',
+            email_rules: [
+                v => !!v || 'Le mail est obligatoire',
+            ],
+            is_participant: true,
+            access_level: 0,
+            access_level_list: [
+                { id: 1, label: 'Aucun' },
+                { id: 2, label: 'Observateur' },
+                { id: 3, label: 'Manager' },
+            ],
+            access_level_rules: [
+                (v) => !!v || 'Un niveau d\'accès doit être renseigné',
+            ],
+            creation_mode: true,
+        }
+    },
+
+    async mounted() {
+        console.log("mounted...")
+        console.log("this.is_participant mounted", this.is_participant)
+        this.init()
+    },
+    watch: {
+        async campaign_id() {
+            await this.init()
+        },
+        async campaign_user_id() {
+            await this.init()
+        },
+    },
+    methods: {
+
+        ...mapActions({
+            addCampaignUser: 'campaign_store/addCampaignUser',
+            updateCampaignUser: 'campaign_store/updateCampaignUser',
+            getCampaignUser: 'campaign_store/getCampaignUser',
+        }),
+        
+        async init() {
+            if (this.campaign_user_id) {
+                this.creation_mode = false;
+                const campaign_user = await this.getCampaignUser({ campaign_id: this.campaign_id, id: this.campaign_user_id });
+                this.email = campaign_user.email
+                this.access_level = campaign_user.access_level
+                this.is_participant = campaign_user.is_participant
+            } else {
+                this.creation_mode = true;
+                this.$refs.form.reset()
+                this.is_participant = true
+            }
+        },
+
+        async save () {
+
+            if (!this.$refs.form.validate()) {
+                this.$emit('validation_error')
+                return;
+            }
+
+            if (this.campaign_user_id) {
+                await this.updateCampaignUser({
+                    id: this.campaign_user_id,
+                    campaign_id: this.campaign_id,
+                    data: {
+                        access_level: this.access_level,
+                        is_participant: this.is_participant,
+                    }
+                })
+                this.$emit('saved')
+                return;
+            }
+            await this.addCampaignUser({
+                campaign_id: this.campaign_id,
+                data: {
+                    email: this.email,
+                    access_level: this.access_level,
+                    is_participant: this.is_participant,
+                }
+            })
+            this.$emit('saved')
+        },
+
+        reset () {
+            this.$refs.form.reset()
+            this.$emit('reset')
+        },
+
+    },
+
+}
+</script>
---
+++ frontend/src/components/elements/campaign_members/CampaignMembersListElement.vue
@@ -0,0 +1,224 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="campaignUserList"
+    hide-default-footer
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>Membres de la campagne</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="primary"
+          dark
+          class="mb-2"
+          @click="createProposition"
+        >
+            Ajouter un membre
+        </v-btn>
+
+        <v-dialog v-model="dialog" max-width="500px">
+            <campaign-members-create-edit-element
+                v-if="dialog"
+                :campaign_id="campaign_id"
+                :campaign_user_id="editing_campaign_user_id"
+                @reset="close"
+                @saved="saved"
+            />
+        </v-dialog>
+
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="headline">
+                Supprimer membre?
+            </v-card-title>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Annuler</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">Oui</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+      </v-toolbar>
+    </template>
+
+    <template v-slot:[`item.actions`]="{ item }">
+        <!--
+        <router-link 
+            tag="span"
+            style="cursor: pointer"
+            :to="{ name: 'field-setup', params: { module_technical_name: item.technical_name } }"
+        >
+            <v-icon class="mr-2" small>mdi-format-list-bulleted-square</v-icon>
+        </router-link>
+        -->
+        <v-icon small class="mr-2" @click="editItem(item)">
+            mdi-pencil
+        </v-icon>
+        <v-icon small @click="deleteItem(item)">
+            mdi-delete
+        </v-icon>
+    </template>
+
+
+    <template v-slot:[`item.access_level`]="{ item }">
+
+            <v-chip
+                v-if="item.access_level === 2"
+                class="ma-2"
+                color="blue darken-1"
+                outlined
+                small
+            >
+            Observateur
+            </v-chip>
+
+            <v-chip
+                v-if="item.access_level === 3"
+                class="ma-2"
+                color="red"
+                text-color="white"
+                small
+            >
+            Manager
+            </v-chip>
+    </template>
+
+    <template v-slot:[`item.is_participant`]="{ item }">
+
+            <v-chip
+                v-if="item.is_participant === true"
+                class="ma-2"
+                color="blue"
+                outlined
+                small
+            >
+            Participant
+            </v-chip>
+    </template>
+
+    <template v-slot:no-data>
+      <v-btn
+        color="primary"
+        @click="initialize"
+      >
+        Reset
+      </v-btn>
+    </template>
+  </v-data-table>
+
+</template>
+
+<script>
+
+import { mapActions, mapGetters } from 'vuex';
+import CampaignMembersCreateEditElement from '@/components/elements/campaign_members/CampaignMembersCreateEditElement'
+
+  export default {
+    name: 'CampaignMembersListElement',
+    components: {
+        CampaignMembersCreateEditElement,
+    },
+    props: [
+        'campaign_id',
+    ],
+    data: () => ({
+        dialog: false,
+        dialogDelete: false,
+        headers: [
+            {
+                text: 'Email',
+                align: 'start',
+                value: 'email',
+            },
+            {
+                text: 'Participant',
+                value: 'is_participant',
+                sortable: true,
+            },
+            {
+                text: 'Niveau de privilège',
+                value: 'access_level',
+                sortable: true,
+            },
+            {
+                text: 'Actions',
+                value: 'actions',
+                align: 'end',
+                sortable: false,
+            },
+        ],
+        editing_campaign_user_id: null,
+    }),
+    computed: {
+        ...mapGetters({
+            campaignUserList: 'campaign_store/campaignUserList',
+        }),
+        formTitle () {
+            return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+        },
+    },
+    watch: {
+        dialog (val) {
+            val || this.close()
+        },
+        dialogDelete (val) {
+            val || this.closeDelete()
+        },
+        campaign_id () {
+            this.initialize()
+        }
+    },
+    mounted () {
+        this.initialize()
+    },
+    methods: {
+        ...mapActions({
+            loadCampaignUserList: 'campaign_store/loadCampaignUserList',
+            clearCampaignUserList: 'campaign_store/clearCampaignUserList',
+            deleteCampaignUser: 'campaign_store/deleteCampaignUser',
+        }),
+        saved() {
+            this.loadCampaignUserList({ campaign_id: this.campaign_id });
+            this.dialog = false
+        },
+        initialize () {
+            if (this.campaign_id) {
+                this.loadCampaignUserList({ campaign_id: this.campaign_id });
+            } else {
+                this.clearCampaignUserList()
+            }
+        },
+        createProposition() {
+            this.editing_campaign_user_id = null
+            this.dialog = true
+        },
+        editItem (item) {
+            this.dialog = true
+            this.editing_campaign_user_id = item.id
+        },
+        deleteItem (item) {
+            this.editing_campaign_user_id = item.id
+            this.dialogDelete = true
+        },
+        async deleteItemConfirm () {
+            await this.deleteCampaignUser({
+                id: this.editing_campaign_user_id,
+                campaign_id: this.campaign_id
+            })
+            this.loadCampaignUserList({ campaign_id: this.campaign_id });
+            this.closeDelete()
+        },
+        close () {
+            this.dialog = false
+        },
+        closeDelete () {
+            this.dialogDelete = false
+        },
+    },
+  }
+</script>
