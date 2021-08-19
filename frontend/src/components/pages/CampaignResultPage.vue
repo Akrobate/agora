9a3f29c57f038026a1afe4728b11a33dfab954f7
Akrobate <fedorov.artiom@gmail.com>
2021-08-19 22:12:24 +0200

    Added page scafold of campaign result page

---
+++ frontend/src/components/pages/CampaignResultPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+        <h1 class="text-h5 mt-8">Campaign results</h1>
+    </v-container>
+</template>
+
+
+<script>
+export default {
+    name: "CampaignResultPage",
+    props: {
+        campaign_id: Number,
+    },
+}
+</script>
