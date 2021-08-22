cf8a4879858cb35861497c2eb2e255a50389df07
Akrobate <fedorov.artiom@gmail.com>
2021-08-22 14:40:02 +0200

    Fixed indentation in authentication store

--- frontend/src/store/modules/authenticationStore.js
+++ frontend/src/store/modules/authenticationStore.js
@@ -1,38 +1,38 @@
 import {
     user_repository,
 } from '@/repositories'
-  
-  
+
+
 const state = () => ({
     authentication_status: '',
     is_connected: user_repository.isValidLocalStorageToken(),
     token: user_repository.getTokenFromLocalStorageIfIsValid(),
     token_data: user_repository.isValidLocalStorageToken() ?
-      user_repository.decodeToken(user_repository.getTokenFromLocalStorageIfIsValid()) : {},
+    user_repository.decodeToken(user_repository.getTokenFromLocalStorageIfIsValid()) : {},
     user: {},
 })
-  
+
 const getters = {
     isConnected: (state) => state.is_connected,
     authenticationStatus: (state) => state.authentication_status,
     token: (state) => state.token,
     tokenData: (state) => state.token_data,
 }
-  
-const actions = {
 
+const actions = {
+    
     login({ commit }, { email, password }) {
         return new Promise((resolve, reject) => {
             commit('authentication_request')
             return user_repository.login(email, password)
             .then((response) => {
-            user_repository.setTokenLocalStorage(response.token)
-            commit('authentication_success', response.token)
-            return resolve(response)
+                user_repository.setTokenLocalStorage(response.token)
+                commit('authentication_success', response.token)
+                return resolve(response)
             })
             .catch((error) => {
-            commit('authentication_error')
-            return reject(error)
+                commit('authentication_error')
+                return reject(error)
             })
         })
     },
@@ -44,32 +44,32 @@ const actions = {
         commit('authentication_success', token)
     },
 }
-  
-  const mutations = {
+
+const mutations = {
     authentication_request(state) {
-      state.authentication_status = 'loading'
+        state.authentication_status = 'loading'
     },
     authentication_success(state, token) {
-      state.authentication_status = 'success'
-      state.is_connected = true
-      state.token = token
-      state.token_data = user_repository.decodeToken(token);
+        state.authentication_status = 'success'
+        state.is_connected = true
+        state.token = token
+        state.token_data = user_repository.decodeToken(token);
     },
     authentication_error(state) {
-      state.authentication_status = 'error'
-      state.is_connected = false
+        state.authentication_status = 'error'
+        state.is_connected = false
     },
     logout(state) {
-      state.status = ''
-      state.token = null
-      state.is_connected = false
+        state.status = ''
+        state.token = null
+        state.is_connected = false
     },
-  }
-  
-  export default {
+}
+
+export default {
     namespaced: true,
     state,
     getters,
     actions,
     mutations
-  }
+}
