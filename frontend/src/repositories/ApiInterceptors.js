c11fe478b93e07a0405d742d0d07074b74cda595
Akrobate <fedorov.artiom@gmail.com>
2021-08-22 19:54:56 +0200

    Fixed interceptors to renew automaticly token, to manage connection when token expired

--- frontend/src/repositories/ApiInterceptors.js
+++ frontend/src/repositories/ApiInterceptors.js
@@ -1,6 +1,7 @@
 import router from '@/router'
 // import status_code from '@/constants/httpStatusCodes'
 import store from '@/store'
+import moment from 'moment'
 
 const responseSuccess = function(response) {
     // console.log("From interceptor", response)
@@ -8,18 +9,34 @@ const responseSuccess = function(response) {
 }
 
 const responseError = function(error) {
-    console.log("From interceptor error")
-    if (error.status === 401) {
-        router.push({ name: 'LoginPage' })
+    console.log(error);
+    if (error.response.status === 401) {
+        router.push({ name: 'login' })
     }
     return Promise.reject(error)
 }
 
 
-const requestAuthenticate = (config) => {
+const requestAuthenticate = async (config) => {
     const token = store.getters['authentication_store/token']
 
     const is_connected = store.getters['authentication_store/isConnected']
+    const token_data = store.getters['authentication_store/tokenData']
+    
+    // console.log(moment.unix(token_data.iat).toISOString());
+    // console.log(moment.unix(token_data.exp).toISOString());
+    // console.log((moment.unix(moment().unix()).toISOString()));
+    
+     // @todo: This mecanic could move to the store
+    const is_renewing_token = store.getters['authentication_store/isRenewingToken']
+    if (
+        (token_data.exp - moment().unix()) < (10 * 60)
+        && is_renewing_token === false
+        && token_data.exp - moment().unix() < token_data.exp
+    ) {
+        console.log("Refreshing token", (token_data.exp - moment().unix()) / 60);
+        await store.dispatch('authentication_store/renewToken')
+    }
 
     if (is_connected) {
         config.headers = {
