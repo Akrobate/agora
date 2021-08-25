ea4b2d3f2c71b795c07a10d82310828875761bdf
Akrobate <fedorov.artiom@gmail.com>
2021-08-25 12:35:14 +0200

    Managing guest template if guest connection

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,28 @@
 <template>
-  <component :is="layout">
-    <router-view></router-view>
-  </component>
+    <component :is="layout">
+        <router-view></router-view>
+    </component>
 </template>
 
+
 <script>
+import { mapGetters } from 'vuex'
 
 export default {
-  name: 'App',
-
-  data: () => ({
-  }),
-  computed: {
-    layout() {
-      return (this.$route.meta.layout || 'AppLayout')
-    }
-  },
+    name: 'App',
+    data: () => ({
+    }),
+    computed: {
+        ...mapGetters({
+            token_data: 'authentication_store/tokenData',
+        }),
+        layout() {
+            console.log('this.token_datathis.token_datathis.token_datathis.token_data', this.token_data)
+            if (this.token_data.access_type === 'guest') {
+                return 'DefaultLayout'
+            }
+            return (this.$route.meta.layout || 'AppLayout')
+        }
+    },
 };
 </script>
