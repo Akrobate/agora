e7371f3c44279824a4fa4c1b909dc05b3ee75d9b
Akrobate <fedorov.artiom@gmail.com>
2021-08-28 04:31:12 +0200

    Added UserRegistrationDataAccess functional test

--- backend/test/functional/UserRegistrationDataAccess.functional.test.js
+++ backend/test/functional/UserRegistrationDataAccess.functional.test.js
@@ -28,31 +28,75 @@ describe('UserRegistrationDataAccess', () => {
         email: `artiom.fedorov@${v4()}.com`,
     };
 
+    const guest_user_seed = {
+        id: 200,
+        email: 'guest.user@test.com',
+    };
+
     before(async () => {
         await DataSeeder.truncate('UserRepository');
         await DataSeeder.createUserHashPassword(user_seed);
-
+        await DataSeeder.create('UserRepository', guest_user_seed);
     });
 
 
-    it('Should be able to register a user', async () => {
-        const user_data = {
-            first_name: 'Artiom',
-            last_name: 'Fedorov',
-            password: 'ShouldHaveLettersDigitsAndAtLeast8chars',
-            email: `artiom.fedorov@${v4()}.com`,
-        };
+    describe('Registration tests', () => {
 
-        await superApp
-            .post('/api/v1/users/register')
-            .send(user_data)
-            .expect(HTTP_CODE.CREATED)
-            .expect((response) => {
-                expect(response.body).to.have.property('id');
-                expect(response.body).to.have.property('email', user_data.email);
-                expect(response.body).to.have.property('first_name', user_data.first_name);
-                expect(response.body).to.have.property('last_name', user_data.last_name);
-            });
+        it('Should be able to register a user', async () => {
+            const user_data = {
+                first_name: 'Artiom',
+                last_name: 'Fedorov',
+                password: 'ShouldHaveLettersDigitsAndAtLeast8chars',
+                email: `artiom.fedorov@${v4()}.com`,
+            };
+            await superApp
+                .post('/api/v1/users/register')
+                .send(user_data)
+                .expect(HTTP_CODE.CREATED)
+                .expect((response) => {
+                    expect(response.body).to.have.property('id');
+                    expect(response.body).to.have.property('email', user_data.email);
+                    expect(response.body).to.have.property('first_name', user_data.first_name);
+                    expect(response.body).to.have.property('last_name', user_data.last_name);
+                });
+        });
+
+
+        it('Should not be able to register an existing and registred user', async () => {
+            await superApp
+                .post('/api/v1/users/register')
+                .send({
+                    first_name: user_seed.first_name,
+                    last_name: user_seed.last_name,
+                    password: user_seed.password,
+                    email: user_seed.email,
+                })
+                .expect(HTTP_CODE.BAD_REQUEST)
+                .expect((response) => {
+                    expect(response.body).to.be.an('Object');
+                    expect(response.body).to.have.property('message', 'Email already exists');
+                });
+        });
+
+
+        it('Should be able to register an existing and guest user', async () => {
+            const guest_registration_data = {
+                first_name: `first_name ${v4()}`,
+                last_name: `last_name ${v4()}`,
+                password: 'Azert987654',
+                email: guest_user_seed.email,
+            };
+            await superApp
+                .post('/api/v1/users/register')
+                .send(guest_registration_data)
+                .expect(HTTP_CODE.CREATED)
+                .expect((response) => {
+                    expect(response.body).to.have.property('email', guest_registration_data.email);
+                    expect(response.body).to.have.property('first_name', guest_registration_data.first_name);
+                    expect(response.body).to.have.property('last_name', guest_registration_data.last_name);
+                });
+        });
+
     });
 
 
