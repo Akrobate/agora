421f93c235c5500220cc28af850c4c2909d4aaec
Akrobate <fedorov.artiom@gmail.com>
2021-08-29 16:58:35 +0200

    Added guest app layout

---
+++ frontend/src/components/layouts/GuestAppLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app>
+        <guest-app-bar />
+        <v-main>
+            <slot />
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import GuestAppBar from '@/components/elements/GuestAppBar';
+
+export default {
+    components: {
+        GuestAppBar,
+    },
+};
+
+</script>
