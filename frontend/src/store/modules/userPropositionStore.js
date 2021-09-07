23551019a762ab63228df381ae311d77490a05e0
Akrobate <fedorov.artiom@gmail.com>
2021-09-07 13:48:19 +0200

    Added loadOwnPropositionResultList to the store

--- frontend/src/store/modules/userPropositionStore.js
+++ frontend/src/store/modules/userPropositionStore.js
@@ -27,6 +27,14 @@ const actions = {
         } = await user_proposition_repository.getPropositionResults(campaign_id, user_id_list, algorithm)
         commit('set_proposition_result_list', proposition_result_list)
         return proposition_result_list
+    },
+
+    async loadOwnPropositionResultList({ commit }, { campaign_id  }) {
+        const {
+            proposition_result_list,
+        } = await user_proposition_repository.getOwnRanking(campaign_id)
+        commit('set_proposition_result_list', proposition_result_list)
+        return proposition_result_list
     }
 }
 
