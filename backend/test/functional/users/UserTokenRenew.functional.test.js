7ed5059cfede441daade87fbb4ccd15f72ad4f2e
Akrobate <fedorov.artiom@gmail.com>
2021-09-09 13:38:33 +0200

    Moved and cleaned UserTokenRenew functional test

--- backend/test/functional/users/UserTokenRenew.functional.test.js
+++ backend/test/functional/users/UserTokenRenew.functional.test.js
@@ -9,17 +9,17 @@ const {
 
 const {
     app,
-} = require('../../src/app');
+} = require('../../../src/app');
 
 const {
     DataSeeder,
-} = require('../helpers/DataSeeder');
+} = require('../../helpers/DataSeeder');
 
 const superApp = superTest(app);
 
 describe('UserTokenRenew', () => {
 
-    const manager_user_seed = {
+    const user_seed = {
         id: 100,
         password: 'ShouldHaveLettersDigitsAndAtLeast8chars1',
         email: 'manager.user@test.com',
@@ -28,16 +28,16 @@ describe('UserTokenRenew', () => {
 
     before(async () => {
         await DataSeeder.truncate('UserRepository');
-        await DataSeeder.createUserHashPassword(manager_user_seed);
+        await DataSeeder.createUserHashPassword(user_seed);
     });
 
 
-    it('Manager should be able to login and one second later renew token', async () => {
+    it('User should be able to login and one second later renew token', async () => {
         await superApp
             .post('/api/v1/users/login')
             .send({
-                email: manager_user_seed.email,
-                password: manager_user_seed.password,
+                email: user_seed.email,
+                password: user_seed.password,
             })
             .expect(HTTP_CODE.OK)
             .expect((response) => {
@@ -46,19 +46,19 @@ describe('UserTokenRenew', () => {
                 expect(decoded).to.be.an('Object');
                 expect(decoded).to.have.property('exp');
                 expect(decoded).to.have.property('iat');
-                expect(decoded).to.have.property('email', manager_user_seed.email);
+                expect(decoded).to.have.property('email', user_seed.email);
             });
 
         await superApp
             .post('/api/v1/users/token/renew')
-            .set('Authorization', `Bearer ${DataSeeder.getJwtFullAccessToken(manager_user_seed)}`)
+            .set('Authorization', `Bearer ${DataSeeder.getJwtFullAccessToken(user_seed)}`)
             .expect(HTTP_CODE.OK)
             .expect((response) => {
                 const decoded = DataSeeder.decodeJwt(response.body.token);
                 expect(decoded).to.be.an('Object');
                 expect(decoded).to.have.property('exp');
                 expect(decoded).to.have.property('iat');
-                expect(decoded).to.have.property('email', manager_user_seed.email);
+                expect(decoded).to.have.property('email', user_seed.email);
             });
     });
 
