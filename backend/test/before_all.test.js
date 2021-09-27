e2364dd6c3ffcbedcf9188a8bfd4e94afbc902dd
Akrobate <fedorov.artiom@gmail.com>
2021-09-27 13:10:33 +0200

    Removed stub

--- backend/test/before_all.test.js
+++ backend/test/before_all.test.js
@@ -4,12 +4,12 @@ const {
     configuration,
 } = require('../src/configuration');
 
-const test_database_name = 'test-database';
-configuration.storage.mysql.database_name = test_database_name;
+// const test_database_name = 'test-database';
+// configuration.storage.mysql.database_name = test_database_name;
 
 before((done) => {
     console.log('======================================');
-    console.log('Database forced to be:', test_database_name);
+    console.log('Database forced to be:', configuration.storage.mysql.database_name);
     console.log('======================================');
 
     // console.log('configuration', configuration)
