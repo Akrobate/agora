3f052d4f9a0f56f226d5fc8466d1ae5ce0030d8c
Akrobate <fedorov.artiom@gmail.com>
2021-09-29 13:15:34 +0200

    Cleaned after all

--- backend/test/before_all.test.js
+++ backend/test/before_all.test.js
@@ -4,15 +4,9 @@ const {
     configuration,
 } = require('../src/configuration');
 
-// const test_database_name = 'test-database';
-// configuration.storage.mysql.database_name = test_database_name;
-
 before((done) => {
     console.log('======================================');
     console.log('Database forced to be:', configuration.storage.mysql.database_name);
     console.log('======================================');
-
-    // console.log('configuration', configuration)
-
     done();
 });
