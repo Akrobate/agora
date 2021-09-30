d4fbc72bb22d61898e638bfd708136814fec4ba5
Akrobate <fedorov.artiom@gmail.com>
2021-09-30 13:51:53 +0200

    Added inclusion file for seeds to be able to split seeds files

---
+++ backend/test/test_seeds/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const {
+    manager_user_seed,
+    campaign_seed,
+    manager_campaign_user_seed,
+    guest_user_seed,
+    guest_campaign_user_seed,
+    guest_campaign_user_to_delete_seed,
+    guest_user_to_delete_seed,
+    campaign_user_status_1_seed,
+    campaign_user_status_2_seed,
+    proposition_1_seed,
+    proposition_2_seed,
+    proposition_3_seed,
+    guest_user_proposition_result_1,
+    guest_user_proposition_result_2,
+    guest_user_proposition_result_3,
+    manager_user_proposition_result_1,
+    manager_user_proposition_result_2,
+    manager_user_proposition_result_3,
+} = require('./test_data_seeds');
+
+
+module.exports = {
+    manager_user_seed,
+    campaign_seed,
+    manager_campaign_user_seed,
+    guest_user_seed,
+    guest_campaign_user_seed,
+    guest_campaign_user_to_delete_seed,
+    guest_user_to_delete_seed,
+    campaign_user_status_1_seed,
+    campaign_user_status_2_seed,
+    proposition_1_seed,
+    proposition_2_seed,
+    proposition_3_seed,
+    guest_user_proposition_result_1,
+    guest_user_proposition_result_2,
+    guest_user_proposition_result_3,
+    manager_user_proposition_result_1,
+    manager_user_proposition_result_2,
+    manager_user_proposition_result_3,
+};
