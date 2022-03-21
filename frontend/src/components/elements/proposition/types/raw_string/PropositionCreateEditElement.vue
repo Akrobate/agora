d2896c966f23ea846a6fe567360a647db83cdd42
Akrobate <fedorov.artiom@gmail.com>
2022-03-21 18:05:28 +0100

    Added raw string create / edit component

---
+++ frontend/src/components/elements/proposition/types/raw_string/PropositionCreateEditElement.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <v-text-field
+            v-model="payload"
+            :counter="255"
+            :rules="payload_rules"
+            label="Contenu de la proposition"
+            required
+        ></v-text-field>
+    </div>
+</template>
+
+<script>
+
+// import store from '@/store'
+
+export default {
+    name: 'CreateEditElement',
+    data: () => ({
+        valid: true,
+        payload: '',
+        payload_rules: [
+            v => !!v || 'Le contenu de la proposition est obligatoire',
+        ],
+    }),
+    computed: { 
+    },
+    mounted() {
+    },
+    methods: {
+    },
+}
+</script>
