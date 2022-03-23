473e180542e14a55e72a877c51fcbef86e4c8a9a
Akrobate <fedorov.artiom@gmail.com>
2022-03-23 13:54:07 +0100

    Added types descriptions and lables in propositionTypes

--- frontend/src/components/elements/proposition/types/propositionTypes.js
+++ frontend/src/components/elements/proposition/types/propositionTypes.js
@@ -2,9 +2,13 @@
 
 const propositionTypes = [
     {
+        description: 'One line proposition',
+        label: 'Raw string',
         type: 'raw_string',
     },
     {
+        description: 'One line and description',
+        label: 'Markdown',
         type: 'markdown',
     },
 ]
