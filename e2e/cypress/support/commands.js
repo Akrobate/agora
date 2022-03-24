127d251899e5e1e07159cd2c7c34b43c8cc3464d
Akrobate <fedorov.artiom@gmail.com>
2022-03-24 01:00:21 +0100

    Added login command

---
+++ e2e/cypress/support/commands.js
@@ -0,0 +1,40 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (email, password) => {
+    cy.request({
+        method: 'POST',
+        url: 'http://192.168.1.11:8085/api/v1/users/login',
+        body: {
+            email,
+            password,
+        }
+    })
+    .then((login_response) => {
+        window.localStorage.setItem('token', login_response.body.token);
+    })
+});
+
