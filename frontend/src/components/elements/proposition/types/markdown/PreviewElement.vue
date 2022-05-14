ca5244006b9fe17bda017f9847e0d5516c3b3ba1
Akrobate <fedorov.artiom@gmail.com>
2022-05-14 14:52:59 +0200

    Finished making Preview markdown element

--- frontend/src/components/elements/proposition/types/markdown/PreviewElement.vue
+++ frontend/src/components/elements/proposition/types/markdown/PreviewElement.vue
@@ -1,10 +1,9 @@
 <template>
-    <div>
-        {{ payload_html }}
-    </div>
+    <div v-html="payload_html" class="markdown_preview" />
 </template>
 
 <script>
+import { marked } from 'marked'
 export default {
     name: 'PreviewElement',
     props: {
@@ -14,12 +13,17 @@ export default {
         payload_html: '',
     }),
     mounted() {
-
+        this.updatePayloadHtml()
     },
     watch: {
         payload() {
-
+            this.updatePayloadHtml()
         },
     },
+    methods: {
+        updatePayloadHtml() {
+            this.payload_html = marked.parse(this.payload)
+        },
+    }
 }
 </script>
