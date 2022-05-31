61c788635d94276e9c95ab9d0333f762728f32d1
Akrobate <fedorov.artiom@gmail.com>
2022-05-31 13:26:58 +0200

    Added campaign to delete seed, added test for acl remove campaign by guest

--- backend/test/functional/CampaignDelete.functional.test.js
+++ backend/test/functional/CampaignDelete.functional.test.js
@@ -15,30 +15,75 @@ const {
     DataSeeder,
 } = require('../test_helpers/DataSeeder');
 
+
+const {
+    manager_user_seed,
+    campaign_seed,
+    manager_campaign_user_seed,
+    guest_user_seed,
+    guest_campaign_user_seed,
+    campaign_user_status_1_seed,
+    campaign_user_status_2_seed,
+    proposition_1_seed,
+    proposition_2_seed,
+    proposition_3_seed,
+    guest_user_proposition_result_1,
+    guest_user_proposition_result_2,
+    guest_user_proposition_result_3,
+    manager_user_proposition_result_1,
+    manager_user_proposition_result_2,
+    manager_user_proposition_result_3,
+} = require('../test_seeds/test_data_seeds');
+
 const superApp = superTest(app);
 
-describe('[WIP] CampaignDelete', () => {
-
-    const user_seed = {
-        id: 100,
-        password: 'ShouldHaveLettersDigitsAndAtLeast8chars1',
-        email: 'artiom.fedorov@test.com',
-    };
+describe.only('[WIP] CampaignDelete', () => {
 
     before(async () => {
 
         await DataSeeder.truncate('CampaignRepository');
+        await DataSeeder.truncate('CampaignUserRepository');
+        await DataSeeder.truncate('CampaignUserStatusRepository');
         await DataSeeder.truncate('UserRepository');
-        await DataSeeder.createUserHashPassword(user_seed);
+        await DataSeeder.truncate('PropositionRepository');
+        await DataSeeder.truncate('UserPropositionResultRepository');
+
+        await DataSeeder.createUserHashPassword(manager_user_seed);
+        await DataSeeder.create('CampaignRepository', campaign_seed);
+        await DataSeeder.create('CampaignUserRepository', manager_campaign_user_seed);
+
+        await DataSeeder.create('UserRepository', guest_user_seed);
+        await DataSeeder.create('CampaignUserRepository', guest_campaign_user_seed);
+
+        await DataSeeder.create('PropositionRepository', proposition_1_seed);
+        await DataSeeder.create('PropositionRepository', proposition_2_seed);
+        await DataSeeder.create('PropositionRepository', proposition_3_seed);
+
+        await DataSeeder.create('UserPropositionResultRepository', manager_user_proposition_result_1);
+        await DataSeeder.create('UserPropositionResultRepository', manager_user_proposition_result_2);
+        await DataSeeder.create('UserPropositionResultRepository', manager_user_proposition_result_3);
+
+        await DataSeeder.create('UserPropositionResultRepository', guest_user_proposition_result_1);
+        await DataSeeder.create('UserPropositionResultRepository', guest_user_proposition_result_2);
+        await DataSeeder.create('UserPropositionResultRepository', guest_user_proposition_result_3);
+
+        await DataSeeder.create('CampaignUserStatusRepository', campaign_user_status_1_seed);
+        await DataSeeder.create('CampaignUserStatusRepository', campaign_user_status_2_seed);
 
     });
 
 
-    it.skip('[WIP] Should be able to delete a campaign', async () => {
-        const campaign_id = 10000000000;
+    it.skip('[WIP] Manager user should be able to delete a campaign', async () => {
         await superApp
-            .delete(`/api/v1/campaigns/${campaign_id}`)
-            .set('Authorization', `Bearer ${DataSeeder.getJwtFullAccessToken(user_seed)}`)
+            .delete(`/api/v1/campaigns/${campaign_seed.id}`)
+            .set('Authorization', `Bearer ${DataSeeder.getJwtFullAccessToken(manager_user_seed)}`)
+            .expect(HTTP_CODE.OK);
+    });
+
+    it.skip('[WIP] Guest user should not be able to delete a campaign', async () => {
+        await superApp
+            .delete(`/api/v1/campaigns/${campaign_seed.id}`)
+            .set('Authorization', `Bearer ${DataSeeder.getJwtFullAccessToken(guest_user_seed)}`)
             .expect(HTTP_CODE.OK);
     });
 });
