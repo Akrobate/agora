1fca560a8b2b35d8b181a48f8554ab77bf72f149
Akrobate <fedorov.artiom@gmail.com>
2022-06-29 03:14:14 +0200

    Covered with tests new function

--- backend/test/functional/CampaignUpdateFinished.functional.test.js
+++ backend/test/functional/CampaignUpdateFinished.functional.test.js
@@ -18,7 +18,7 @@ const {
     DataSeeder,
 } = require('../test_helpers/DataSeeder');
 
-describe.only('CampaignUpdateFinished', () => {
+describe('updateCampaignIfFinished method', () => {
 
     const campaign_service = CampaignService.getInstance();
     const campaign_repository = CampaignRepository.getInstance();
@@ -38,26 +38,45 @@ describe.only('CampaignUpdateFinished', () => {
             .toISOString(),
     };
 
+    const campaign_seed_not_expired = {
+        id: 4,
+        title: 'Title of campaignsss TIME NOT EXPIRED',
+        description: 'Somethings',
+        proposition_type: 'Proposition_type',
+        campaign_status: CampaignRepository.STATUS_IN_PROGRESS, // STATUS_IN_PROGRESS
+        owner_user_id: 10001,
+        start_date: moment()
+            .subtract(8, 'days')
+            .toISOString(),
+        end_date: moment()
+            .add(1, 'days')
+            .toISOString(),
+    };
+
     before(async () => {
-
         await DataSeeder.truncateAll();
+        await DataSeeder.create('CampaignRepository', campaign_seed_not_expired);
         await DataSeeder.create('CampaignRepository', campaign_seed_expired);
-
     });
 
-    it('UpdateFinished method should update campaign status if finished', async () => {
+    it('Should update status of finished campaign', async () => {
         await campaign_service.updateCampaignIfFinished(campaign_seed_expired.id);
-
-        const updated_campaign_list = await campaign_repository.search({
-            id_list: [
-                campaign_seed_expired.id,
-            ],
-        });
         const [
             updated_campaign,
-        ] = updated_campaign_list;
-        console.log(updated_campaign);
+        ] = await campaign_repository.search({
+            id: campaign_seed_expired.id,
+        });
         expect(updated_campaign).to.have.property('campaign_status', CampaignRepository.STATUS_FINISHED);
+    });
+
+    it('Should not update a not finished campaign', async () => {
+        await campaign_service.updateCampaignIfFinished(campaign_seed_not_expired.id);
+        const [
+            updated_campaign,
+        ] = await campaign_repository.search({
+            id: campaign_seed_not_expired.id,
+        });
+        expect(updated_campaign).to.have.property('campaign_status', CampaignRepository.STATUS_IN_PROGRESS);
     });
 
 });
