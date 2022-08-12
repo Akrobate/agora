41c8b79bc49ab50c91f9aaa7401c88ee0e82a97b
Akrobate <fedorov.artiom@gmail.com>
2022-08-12 22:12:00 +0200

    Fixes indentation in html, added i18n

--- frontend/src/components/elements/campaign/CampaignListElement.vue
+++ frontend/src/components/elements/campaign/CampaignListElement.vue
@@ -1,103 +1,102 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="campaignData"
-    class="elevation-1"
-  >
-    <template v-slot:top>
+    <v-data-table
+        :headers="headers"
+        :items="campaignData"
+        class="elevation-1"
+    >
+        <template v-slot:top>
+            <v-toolbar flat>
+                <v-toolbar-title>{{ list_title }}</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-btn
+                    v-if="campaign_status === 'draft'"
+                    color="primary"
+                    dark
+                    class="mb-2"
+                    :to="{'name': 'campaign-create', params: { campaign_status: 'create'}}"
+                    :exact="true"
+                >
+                    {{ $t('create_new_campaign_button') }}
+                </v-btn>
 
-      <v-toolbar flat>
-        <v-toolbar-title>{{ list_title }}</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn
-          v-if="campaign_status === 'draft'"
-          color="primary"
-          dark
-          class="mb-2"
-          :to="{'name': 'campaign-create', params: { campaign_status: 'create'}}"
-          :exact="true"
-        >
-          Nouvelle campagne
-        </v-btn>
+                <v-dialog v-model="dialog_delete" max-width="400px">
+                    <v-card>
+                        <v-card-title class="headline">{{ $t('delete_campaign_modal_title') }}</v-card-title>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" text @click="closeDelete">{{ $t('cancel_button') }}</v-btn>
+                            <v-btn color="blue darken-1" text @click="deleteItemConfirm">{{ $t('yes_button') }}</v-btn>
+                            <v-spacer></v-spacer>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
 
-        <v-dialog v-model="dialog_delete" max-width="400px">
-          <v-card>
-            <v-card-title class="headline">Supprimer la campagne?</v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Annuler</v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm">Oui</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
+            </v-toolbar>
+        </template>
 
-      </v-toolbar>
-    </template>
+        <template v-slot:[`item.actions`]="{ item }">
 
-    <template v-slot:[`item.actions`]="{ item }">
+            <router-link
+                v-if="item.user_is_participant && item.campaign_status === CAMPAIGN_STATUS.IN_PROGRESS"
+                tag="span"
+                style="cursor: pointer"
+                :to="{ name: 'campaign-participate', params: { campaign_id: item.id } }"
+            >
+                <v-icon class="mr-2" color="green">mdi-play-circle</v-icon>
+            </router-link>
 
-        <router-link
-            v-if="item.user_is_participant && item.campaign_status === CAMPAIGN_STATUS.IN_PROGRESS"
-            tag="span"
-            style="cursor: pointer"
-            :to="{ name: 'campaign-participate', params: { campaign_id: item.id } }"
-        >
-            <v-icon class="mr-2" color="green">mdi-play-circle</v-icon>
-        </router-link>
+            <router-link
+                v-if="
+                    item.user_is_participant
+                    && [CAMPAIGN_STATUS.IN_PROGRESS, CAMPAIGN_STATUS.FINISHED].includes(item.campaign_status)
+                    && (
+                        item.user_access_level === USER_ACCESS_LEVEL.MANAGER
+                        || item.user_access_level === USER_ACCESS_LEVEL.OBSERVER
+                    )
+                "
+                tag="span"
+                style="cursor: pointer"
+                :to="{ name: 'campaign-result', params: { campaign_id: item.id } }"
+            >
+                <v-icon class="mr-2" >mdi-format-list-bulleted-square</v-icon>
+            </router-link>
 
-        <router-link
-            v-if="
-                item.user_is_participant
-                && [CAMPAIGN_STATUS.IN_PROGRESS, CAMPAIGN_STATUS.FINISHED].includes(item.campaign_status)
-                && (
-                    item.user_access_level === USER_ACCESS_LEVEL.MANAGER
-                    || item.user_access_level === USER_ACCESS_LEVEL.OBSERVER
-                )
-            "
-            tag="span"
-            style="cursor: pointer"
-            :to="{ name: 'campaign-result', params: { campaign_id: item.id } }"
-        >
-            <v-icon class="mr-2" >mdi-format-list-bulleted-square</v-icon>
-        </router-link>
+            <router-link
+                v-if="item.user_access_level === USER_ACCESS_LEVEL.MANAGER"
+                tag="span"
+                style="cursor: pointer"
+                :to="{ name: 'campaign-edit', params: { id: item.id } }"
+            >
+                <v-icon class="mr-2" color="blue">mdi-pencil</v-icon>
+            </router-link>
 
-        <router-link
-            v-if="item.user_access_level === USER_ACCESS_LEVEL.MANAGER"
-            tag="span"
-            style="cursor: pointer"
-            :to="{ name: 'campaign-edit', params: { id: item.id } }"
-        >
-            <v-icon class="mr-2" color="blue">mdi-pencil</v-icon>
-        </router-link>
-
-        <v-icon
-            v-if="item.user_access_level === USER_ACCESS_LEVEL.MANAGER"
-            color="red"
-            @click="deleteItem(item.id)"
-        >
-            mdi-delete
-        </v-icon>
-    </template>
+            <v-icon
+                v-if="item.user_access_level === USER_ACCESS_LEVEL.MANAGER"
+                color="red"
+                @click="deleteItem(item.id)"
+            >
+                mdi-delete
+            </v-icon>
+        </template>
 
 
-    <template v-slot:[`item.start`]="{ item }">
-        {{item.start_date | formatDate}}
-    </template>
+        <template v-slot:[`item.start`]="{ item }">
+            {{item.start_date | formatDate}}
+        </template>
 
-    <template v-slot:[`item.end`]="{ item }">
-        {{item.end_date | formatDate}}
-    </template>
+        <template v-slot:[`item.end`]="{ item }">
+            {{item.end_date | formatDate}}
+        </template>
 
-    <template v-slot:no-data>
-      <v-btn
-        color="primary"
-        @click="initialize"
-      >
-        Actualiser
-      </v-btn>
-    </template>
-  </v-data-table>
+        <template v-slot:no-data>
+            <v-btn
+                color="primary"
+                @click="initialize"
+            >
+                Actualiser
+            </v-btn>
+        </template>
+    </v-data-table>
 </template>
 
 <script>
@@ -204,7 +203,6 @@ import { CAMPAIGN_STATUS, USER_ACCESS_LEVEL } from '@/constants'
         },
         async deleteItemConfirm() {
             await this.deleteCampaign(this.to_delete_campaign_id)
-            // this.initialize()
             this.closeDelete()
         },
         closeDelete() {
@@ -213,3 +211,12 @@ import { CAMPAIGN_STATUS, USER_ACCESS_LEVEL } from '@/constants'
     },
   }
 </script>
+
+<i18n locale='fr'>
+{
+    "create_new_campaign_button": "Nouvelle campagne",
+    "delete_campaign_modal_title": "Supprimer la campagne?",
+    "cancel_button": "Annuler",
+    "yes_button": "Oui"
+}
+</i18n>
