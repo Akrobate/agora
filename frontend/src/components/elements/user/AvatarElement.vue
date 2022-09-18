2580d4dc8ec7ee0a58d8f9211c47745919fc90a7
Akrobate <fedorov.artiom@gmail.com>
2022-09-18 14:10:07 +0200

    Added gravatar element scaffold

---
+++ frontend/src/components/elements/user/AvatarElement.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-card>
+    </v-card>
+</template>
+
+<script>
+
+// Using gravatar, if doesn't gravatar found then own picto
+
+
+export default {
+    data() {
+        return {}
+    },
+    computed: {
+    },
+    async mounted() {
+    },
+}
+
+</script>
+
+<i18n locale="fr">
+{
+}
+</i18n>
