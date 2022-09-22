cdeb4a48f0226f6982d78b5cd5866c661108bc9a
Akrobate <fedorov.artiom@gmail.com>
2022-09-22 13:12:48 +0200

    Cleaned app bar

--- frontend/src/components/elements/AppBar.vue
+++ frontend/src/components/elements/AppBar.vue
@@ -20,8 +20,12 @@
             <v-icon>mdi-information</v-icon>
         </v-btn>
 
-        <v-btn icon :to="{ name: 'about'}">
-            <v-icon>mdi-cog</v-icon>
+        <v-btn icon :to="{ name: 'user-profile'}">
+            <avatar-element size='25' :email="email" />
+        </v-btn>
+
+        <v-btn icon @click="logout()">
+            <v-icon>mdi-logout</v-icon>
         </v-btn>
     </v-app-bar>
 </template>
@@ -29,22 +33,37 @@
 <script>
 
 import { mapActions, mapGetters } from 'vuex'
+import AvatarElement from '@/components/elements/user/AvatarElement'
 
 export default {
-    data: () => ({}),
+    data: () => ({
+        email: '',
+    }),
+    components: {
+        AvatarElement,
+    },
+    mounted() {
+        this.email = this.token_data.email
+    },
     methods: {
         ...mapActions({
             setOpenedDrawer: 'app_layout_store/setOpenedDrawer',
+            Authenticationlogout: 'authentication_store/logout',
         }),
         toggleDrawerState() {
             this.setOpenedDrawer(!this.isOpenedDrawer)
+        },
+        logout() {
+            this.Authenticationlogout();
+            this.$router.push({name: 'login'});
         },
     },
     computed: {
         ...mapGetters({
             isOpenedDrawer: 'app_layout_store/isOpenedDrawer',
+            token_data: 'authentication_store/tokenData',
         }),
-    }
+    },
 }
 </script>
 
