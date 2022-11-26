a543b668decbfd85b19ff23f1fc8c4b717b1f036
Akrobate <fedorov.artiom@gmail.com>
2022-11-26 01:04:12 +0100

    Added users contacts

---
+++ backend/migrations/20221125120000-users_contacts.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+    down: (query_interface) => query_interface
+        .dropTable('users_contacts'),
+    up: (query_interface, sequelize) => query_interface
+        .createTable(
+            'users_contacts',
+            {
+                id: {
+                    allowNull: false,
+                    autoIncrement: true,
+                    primaryKey: true,
+                    type: sequelize.INTEGER.UNSIGNED,
+                },
+                user_id: {
+                    allowNull: false,
+                    type: sequelize.INTEGER.UNSIGNED,
+                    unique: false,
+                },
+                contact_user_id: {
+                    allowNull: false,
+                    type: sequelize.INTEGER.UNSIGNED,
+                    unique: false,
+                },
+                created_at: {
+                    allowNull: false,
+                    defaultValue: sequelize.literal('NOW()'),
+                    type: sequelize.DATE,
+                },
+                updated_at: {
+                    allowNull: false,
+                    defaultValue: sequelize.literal('NOW()'),
+                    type: sequelize.DATE,
+                },
+            },
+            {
+                charset: 'utf8',
+                collate: 'utf8_general_ci',
+            }
+        ),
+};
