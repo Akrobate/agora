238bf9017cfc07b17d317712bbe9c202e39e7f53
Akrobate <fedorov.artiom@gmail.com>
2023-01-04 14:03:27 +0100

    Working on creation content user tag

--- backend/test/functional/user_contacts/UserContactTagManagement.functional.test.js
+++ backend/test/functional/user_contacts/UserContactTagManagement.functional.test.js
@@ -28,14 +28,23 @@ const {
     manager_seed_contact_tag_1,
     manager_seed_contact_tag_2,
     manager_seed_2_contact_tag_1,
+    contact_1_user_seed,
+    contact_2_user_seed,
+    contact_3_user_seed,
+    contact_4_user_seed,
 } = require('../../test_seeds/test_data_seeds');
 
 const superApp = superTest(app);
 
-describe('UserContactTagManagement', () => {
+describe.only('User Contact Management', () => {
 
     beforeEach(async () => {
         await DataSeeder.truncateAll();
+
+        await DataSeeder.create('UserRepository', contact_1_user_seed);
+        await DataSeeder.create('UserRepository', contact_2_user_seed);
+        await DataSeeder.create('UserRepository', contact_3_user_seed);
+        await DataSeeder.create('UserRepository', contact_4_user_seed);
 
         await DataSeeder.create('CampaignRepository', campaign_seed);
 
@@ -52,6 +61,31 @@ describe('UserContactTagManagement', () => {
         await DataSeeder.create('ContactTagRepository', manager_seed_contact_tag_2);
         await DataSeeder.create('ContactTagRepository', manager_seed_2_contact_tag_1);
 
+    });
+
+
+    describe('Create content in user tag', () => {
+        it('Should be able to add a user contact', async () => {
+            const content_to_create = {
+                tag_id: 'My new created tag',
+                user_id: manager_user_seed.id,
+                contact_id_list: [
+                    contact_1_user_seed.id,
+                    contact_2_user_seed.id,
+                    contact_3_user_seed.id,
+                    contact_4_user_seed.id,
+                ],
+            };
+
+            await superApp
+                .post(`${url_prefix}/contacts`)
+                .set('Authorization', `Bearer ${DataSeeder.getJwtFullAccessToken(manager_user_seed)}`)
+                .send(content_to_create)
+                .expect(HTTP_CODE.CREATED)
+                .expect((response) => {
+                    expect(response).to.have.property('body');
+                });
+        });
     });
 
 });
