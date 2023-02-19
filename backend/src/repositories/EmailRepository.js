456cbe8cf8a4fc6ed591f0515a711569030d7d25
Akrobate <fedorov.artiom@gmail.com>
2023-02-19 16:05:08 +0100

    Workin on EmailRepository

---
+++ backend/src/repositories/EmailRepository.js
@@ -0,0 +1,159 @@
+'use strict';
+
+const {
+    DataTypes,
+    Op,
+} = require('sequelize');
+
+const {
+    sequelize,
+} = require('./connections/Sequelize');
+
+const {
+    AbstractSequelizeRepository,
+} = require('./AbstractSequelizeRepository');
+
+class EmailRepository extends AbstractSequelizeRepository {
+
+
+    // eslint-disable-next-line require-jsdoc
+    static get STATUS_SENT() {
+        return 1;
+    }
+
+    // eslint-disable-next-line require-jsdoc
+    static get STATUS_TO_SEND() {
+        return 2;
+    }
+
+    /**
+     * @static
+     * @returns {EmailRepository}
+     */
+    static getInstance() {
+        if (EmailRepository.instance === null) {
+            EmailRepository.instance = new EmailRepository(
+                EmailRepository.getModel()
+            );
+        }
+
+        return EmailRepository.instance;
+    }
+
+
+    /**
+     * @returns {Sequelize<Object>}
+     */
+    static getModel() {
+        return sequelize.define(
+            'Campaign',
+            {
+                id: {
+                    allowNull: false,
+                    autoIncrement: true,
+                    primaryKey: true,
+                    type: DataTypes.INTEGER.UNSIGNED,
+                },
+                object: {
+                    allowNull: false,
+                    type: DataTypes.STRING,
+                },
+                body: {
+                    allowNull: true,
+                    type: DataTypes.STRING,
+                },
+                email_status: {
+                    allowNull: false,
+                    type: DataTypes.INTEGER.UNSIGNED,
+                },
+                from_user_id: {
+                    allowNull: false,
+                    type: DataTypes.INTEGER.UNSIGNED,
+                },
+                to_user_id: {
+                    allowNull: false,
+                    type: DataTypes.INTEGER.UNSIGNED,
+                },
+                email_to: {
+                    allowNull: false,
+                    type: DataTypes.STRING,
+                },
+                email_from: {
+                    allowNull: false,
+                    type: DataTypes.STRING,
+                },
+                send_at: {
+                    allowNull: false,
+                    type: DataTypes.DATE,
+                },
+                created_at: {
+                    allowNull: false,
+                    type: DataTypes.DATE,
+                },
+                updated_at: {
+                    allowNull: false,
+                    type: DataTypes.DATE,
+                },
+            },
+            {
+                tableName: 'campaigns',
+                underscored: true,
+                timestamps: false,
+            }
+        );
+    }
+
+    /**
+     * @static
+     * @param {Object} criteria
+     * @returns {Object}
+     */
+    static _formatCriteria(criteria) {
+
+        const {
+            id,
+            id_list,
+            campaign_status_list,
+            end_date_upper_boundary,
+        } = criteria;
+
+
+        const where = {
+            [Op.and]: {},
+        };
+
+        if (id !== undefined) {
+            where[Op.and].id = {
+                [Op.eq]: id,
+            };
+        }
+
+        if (id_list !== undefined) {
+            where[Op.and].id = {
+                [Op.in]: id_list,
+            };
+        }
+
+        if (campaign_status_list !== undefined) {
+            where[Op.and].campaign_status = {
+                [Op.in]: campaign_status_list,
+            };
+        }
+
+        if (end_date_upper_boundary !== undefined) {
+            where[Op.and].end_date = {
+                [Op.lte]: end_date_upper_boundary,
+            };
+        }
+
+        return {
+            where,
+        };
+    }
+}
+
+EmailRepository.instance = null;
+
+module.exports = {
+    EmailRepository,
+};
