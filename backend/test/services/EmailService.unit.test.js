6dfdace0f238649c40f5ac1df88fa582985f1149
Akrobate <fedorov.artiom@gmail.com>
2023-02-27 19:07:22 +0100

    Added first test EmailService create queued mail

--- backend/test/services/EmailService.unit.test.js
+++ backend/test/services/EmailService.unit.test.js
@@ -11,13 +11,30 @@ const {
 } = require('../../src/services');
 
 
-describe('EmailService unit tests', () => {
+describe.only('EmailService unit tests', () => {
 
     beforeEach(async () => {
         await DataSeeder.truncate('EmailRepository');
     });
 
-    it('Should be able to enqueue email', () => {
+    it('Should be able to enqueue email', async () => {
+        const email_service = EmailService.getInstance();
+        const create_data = {
+            to_list: [
+                'to_toto@test.com',
+                'to_toto@test2.com'
+            ],
+            from_email: 'from_email@test.com',
+            from_name: 'FromEmailLabel',
+            from_user_id: 122,
+            to_user_id: 123,
+            subject: 'Email subject',
+            html: '<p>Html content</p>',
+            text: 'Text content',
+        };
+        const email_data = await email_service.createQueuedSendMail(create_data);
+        console.log(email_data);
+        expect(email_data).to.have.property('id');
 
     });
 
