2b963473c7ec429d52aab92401d0d159b35539ee
Akrobate <fedorov.artiom@gmail.com>
2023-03-09 13:45:47 +0100

    Added tests for counts Emails

--- backend/test/repositories/EmailRepository.unit.test.js
+++ backend/test/repositories/EmailRepository.unit.test.js
@@ -18,10 +18,7 @@ describe.only('EmailRepository unit tests', () => {
 
     beforeEach(async () => {
         const create_data = {
-            to_list: [
-                'to_toto@test.com',
-                'to_toto@test2.com',
-            ],
+            email_to: 'to_toto@test.com',
             from_email: 'from_email@test.com',
             from_name: 'FromEmailLabel',
             from_user_id: 122,
@@ -29,16 +26,68 @@ describe.only('EmailRepository unit tests', () => {
             subject: 'Email subject',
             html: '<p>Html content</p>',
             text: 'Text content',
+            email_status: 1,
         };
         await DataSeeder.truncate('EmailRepository');
+        await DataSeeder.create('EmailRepository', create_data);
+        await DataSeeder.create('EmailRepository', create_data);
+        await DataSeeder.create('EmailRepository', create_data);
+
+        await DataSeeder.create('EmailRepository', {
+            ...create_data,
+            email_status: 2,
+        });
+        await DataSeeder.create('EmailRepository', {
+            ...create_data,
+            email_status: 2,
+        });
+        await DataSeeder.create('EmailRepository', {
+            ...create_data,
+            email_status: 3,
+        });
+
     });
 
     it('Should be able to count', async () => {
-    
+        const email_count = await email_repository.count();
+        expect(email_count).to.equal(6);
     });
 
 
     it('Should be able to count by status', async () => {
+        let email_count = 0;
+        email_count = await email_repository.count({
+            email_status: 1,
+        });
+        expect(email_count).to.equal(3);
 
+        email_count = await email_repository.count({
+            email_status: 2,
+        });
+        expect(email_count).to.equal(2);
+
+        email_count = await email_repository.count({
+            email_status: 3,
+        });
+        expect(email_count).to.equal(1);
+    });
+
+    it('Should be able to count by status list', async () => {
+        let email_count = 0;
+        email_count = await email_repository.count({
+            email_status_list: [
+                1,
+                3,
+            ],
+        });
+        expect(email_count).to.equal(4);
+
+        email_count = await email_repository.count({
+            email_status_list: [
+                2,
+                3,
+            ],
+        });
+        expect(email_count).to.equal(3);
     });
 });
