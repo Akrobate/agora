4ccd4f58ca14152b83a5e89fd1eb5b4b0b2567e9
Akrobate <fedorov.artiom@gmail.com>
2023-04-23 02:03:12 +0200

    Declared userContactTagStore

---
+++ frontend/src/store/modules/userContactTagStore.js
@@ -0,0 +1,21 @@
+import {
+    user_contact_tag_store,
+  } from '@/repositories'
+  
+  
+  const state = () => ({})
+  
+  const getters = {}
+  
+  const actions = {}
+  
+  const mutations = {}
+  
+  export default {
+      namespaced: true,
+      state,
+      getters,
+      actions,
+      mutations
+  }
+  
