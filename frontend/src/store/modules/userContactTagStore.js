9e252eee49e07da6ecacae2bc733a1f10e903fb2
Akrobate <fedorov.artiom@gmail.com>
2023-04-26 20:20:23 +0200

    Added stats contact lists

--- frontend/src/store/modules/userContactTagStore.js
+++ frontend/src/store/modules/userContactTagStore.js
@@ -3,7 +3,9 @@ import {
   } from '@/repositories'
   
   
-  const state = () => ({})
+  const state = () => ({
+    user_contact_tag_list: []
+  })
   
   const getters = {}
   
@@ -14,6 +16,11 @@ import {
       } = await user_contact_tag_store.searchContacts(criteria)
       return contact_list;
     },
+    async loadCampaigns({ commit, dispatch }, { criteria = {} } = {}) {
+      const user_contact_tag_list = await dispatch('searchContacts', criteria)
+      commit('set_user_contact_tag_list', user_contact_tag_list)
+      return user_contact_tag_list
+  },
     async searcgTags(_, criteria) {
       const {
         tag_list,
@@ -33,7 +40,11 @@ import {
     },
   }
   
-  const mutations = {}
+  const mutations = {
+    set_user_contact_tag_list(_state, user_contact_tag_list) {
+      _state.user_contact_tag_list = user_contact_tag_list;    
+    },
+  }
   
   export default {
       namespaced: true,
