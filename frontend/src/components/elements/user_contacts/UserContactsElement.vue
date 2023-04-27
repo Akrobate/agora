a8597029951014beecd5acbf7c19fb1b0776dfa3
Akrobate <fedorov.artiom@gmail.com>
2023-04-27 17:04:24 +0200

    Added userContactElement vue file

---
+++ frontend/src/components/elements/user_contacts/UserContactsElement.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1>UserContactsElement</h1>
+</template>
+
+
+<script>
+
+import { mapActions, mapGetters } from 'vuex';
+import AvatarElement from '@/components/elements/user/AvatarElement'
+
+  export default {
+    name: 'UserContactsElement',
+    components: {
+        AvatarElement,
+    },
+    data: () => ({
+    }),
+    computed: {
+        ...mapGetters({
+            userContactTagList: 'user_contact_tag_store/userContactTagList',
+        }),
+    },
+    watch: {
+    },
+    async mounted () {
+        await this.initialize()
+    },
+    methods: {
+        ...mapActions({
+            loadContacts: 'user_contact_tag_store/loadContacts',
+            triggerError: 'snack_bar_store/triggerError',
+            triggerSuccess: 'snack_bar_store/triggerSuccess',
+        }),
+        async initialize () {
+
+        },
+    },
+  }
+</script>
+
+<i18n locale='fr'>
+{
+}
+</i18n>
