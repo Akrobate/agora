43348e534901553fa26f3eb2c22620258ca8bae8
Akrobate <fedorov.artiom@gmail.com>
2023-05-05 18:15:10 +0200

    Fixed indentation in userContactTagStore

--- frontend/src/store/modules/userContactTagStore.js
+++ frontend/src/store/modules/userContactTagStore.js
@@ -1,79 +1,79 @@
 import {
     user_contact_tag_repository,
-  } from '@/repositories'
+} from '@/repositories'
   
   
-  const state = () => ({
-    user_contact_tag_list: [],
-    user_contact_list: [],
-  })
+const state = () => ({
+  user_contact_tag_list: [],
+  user_contact_list: [],
+})
   
-  const getters = {
-    userContactTagList: (_state) => _state.user_contact_tag_list,
-    userContactList: (_state) => _state.user_contact_list,
-  }
+const getters = {
+  userContactTagList: (_state) => _state.user_contact_tag_list,
+  userContactList: (_state) => _state.user_contact_list,
+}
   
-  const actions = {
-    async searchContacts(_, criteria) {
-      const {
-        contact_list,
-      } = await user_contact_tag_repository.searchContacts(criteria)
-      return contact_list;
-    },
-    async loadContacts({ commit, dispatch }, { criteria = {} } = {}) {
-      const user_contact_list = await dispatch('searchContacts', criteria)
-      commit('set_user_contact_list', user_contact_list)
-      return user_contact_list
-    },
-    async searchContactTags(_, criteria) {
-      const {
-        tag_list,
-      } = await user_contact_tag_repository.searchContactsTags(criteria)
-      return tag_list;
-    },
-    async loadContactTags({ commit, dispatch }, { criteria = {} } = {}) {
-      const user_contact_tag_list = await dispatch('searchContactTags', criteria)
-      commit('set_user_contact_tag_list', user_contact_tag_list)
-      return user_contact_tag_list
-    },
-    async searcgTags(_, criteria) {
-      const {
-        tag_list,
-      } = await user_contact_tag_repository.searchContactsTags(criteria)
-      return tag_list;
-    },
-    getContactsTag(_, {
-      id,
-    }) {
-      return user_contact_tag_repository.searchContactsTags(id)
-    },
-    updateContactsTag(_, {
-      id,
-      data,
-    }) {
-      return user_contact_tag_repository.updateContactsTags(id, data)
-    },
-    createContactsTag(_, {
-      data,
-    }) {
-      return user_contact_tag_repository.createContactsTags(data)
-    },
-  }
+const actions = {
+  async searchContacts(_, criteria) {
+    const {
+      contact_list,
+    } = await user_contact_tag_repository.searchContacts(criteria)
+    return contact_list;
+  },
+  async loadContacts({ commit, dispatch }, { criteria = {} } = {}) {
+    const user_contact_list = await dispatch('searchContacts', criteria)
+    commit('set_user_contact_list', user_contact_list)
+    return user_contact_list
+  },
+  async searchContactTags(_, criteria) {
+    const {
+      tag_list,
+    } = await user_contact_tag_repository.searchContactsTags(criteria)
+    return tag_list;
+  },
+  async loadContactTags({ commit, dispatch }, { criteria = {} } = {}) {
+    const user_contact_tag_list = await dispatch('searchContactTags', criteria)
+    commit('set_user_contact_tag_list', user_contact_tag_list)
+    return user_contact_tag_list
+  },
+  async searcgTags(_, criteria) {
+    const {
+      tag_list,
+    } = await user_contact_tag_repository.searchContactsTags(criteria)
+    return tag_list;
+  },
+  getContactsTag(_, {
+    id,
+  }) {
+    return user_contact_tag_repository.searchContactsTags(id)
+  },
+  updateContactsTag(_, {
+    id,
+    data,
+  }) {
+    return user_contact_tag_repository.updateContactsTags(id, data)
+  },
+  createContactsTag(_, {
+    data,
+  }) {
+    return user_contact_tag_repository.createContactsTags(data)
+  },
+}
   
-  const mutations = {
-    set_user_contact_list(_state, user_contact_list) {
-      _state.user_contact_list = user_contact_list;    
-    },
-    set_user_contact_tag_list(_state, user_contact_tag_list) {
-      _state.user_contact_tag_list = user_contact_tag_list;    
-    },
-  }
-  
-  export default {
-      namespaced: true,
-      state,
-      getters,
-      actions,
-      mutations
-  }
+const mutations = {
+  set_user_contact_list(_state, user_contact_list) {
+    _state.user_contact_list = user_contact_list;    
+  },
+  set_user_contact_tag_list(_state, user_contact_tag_list) {
+    _state.user_contact_tag_list = user_contact_tag_list;    
+  },
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
   
