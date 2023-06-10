0f6a243fdedad1133b685a3675841d113b81a65a
Akrobate <fedorov.artiom@gmail.com>
2023-06-10 13:10:55 +0200

    Added ModalWrapper generic component

---
+++ frontend/src/components/elements/ModalWrapper.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-dialog v-model="dialog" max-width="500px">
+        <campaign-members-create-edit-element
+            v-if="dialog"
+            :campaign_id="campaign_id"
+            :campaign_user_id="editing_campaign_user_id"
+            @reset="close"
+            @saved="saved"
+        />
+    </v-dialog>
+</template>
+
+<script>
+
+export default {
+    name: 'ModalWrapper',
+    props: [
+        'campaign_id',
+    ],
+    data: () => ({
+        dialog: false,
+    }),
+    computed: {
+    },
+    watch: {
+        dialog (val) {
+            val || this.close()
+        },
+    },
+    async mounted () {
+    },
+    methods: {
+        close () {
+            this.dialog = false
+        },
+    },
+  }
+</script>
